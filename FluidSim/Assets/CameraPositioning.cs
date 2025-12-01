65bcf8967ca9bb52c71a941e7d3361931ccce17f
vapsik <95504719+vapsik@users.noreply.github.com>
2025-12-01 23:09:47 +0200

    Added a new scene and a script CameraPositioning.cs
    A new script for centered camera initialization
    TODO
    -expand smoke mask channels to allow mixing of different colored dye
    -add obstacle masks
    -camera movement?
    -Red-Black SOR (or multigrid) Poisson solver implementation in compute shaders

---
+++ FluidSim/Assets/CameraPositioning.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPositioning : MonoBehaviour
+{
+
+    public Transform fluidManager;
+
+    FluidSimulator fluidGrid;
+
+    public Camera Camera;
+
+    public bool SetSizeAccordingToGrid = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SetSizeAccordingToGrid){
+            fluidGrid = fluidManager.GetComponent<FluidSimulator>();
+            Camera.orthographicSize = fluidGrid.CellCountX * fluidGrid.CellSize/4;
+
+            //centering
+            transform.position = new Vector3(fluidGrid.CellCountX * fluidGrid.CellSize*0.5f, fluidGrid.CellCountY * fluidGrid.CellSize*0.5f, transform.position.z);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
